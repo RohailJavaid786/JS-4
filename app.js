0dba45ca500b773ff1c7049e4615883b852833cc
RohailJavaid786 <155910153+RohailJavaid786@users.noreply.github.com>
2024-01-28 14:12:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+   
+   
+   //q4-a
+
+   let number = prompt("Enter a number: ");
+
+   for (let i = 1; i <= 10; i++) {
+     console.log(`${number} x ${i} = ${number * i}`);
+   }
+
+
+
+//Q-4-b
+
+let num = 5;
+let factorial = 1;
+let i = 1;
+
+while (i <= number) {
+  factorial *= i;
+  i++;
+}
+
+console.log("The factorial of 5 is: " + factorial);
+
